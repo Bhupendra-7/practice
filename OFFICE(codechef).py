37ac7eaa1bdbf211dbcdcc54a8ed761eed8d4bbf
Bhupendra Vyas <bhupendra.vyas0123@gmail.com>
2025-09-07 15:07:40 +0530

    Practiced

---
+++ OFFICE(codechef).py
@@ -0,0 +1,13 @@
+# cook your dish here
+def findTotal(x,y):
+    work_week = x * 4
+    res = work_week + y
+    return res
+    
+    
+    
+# T = int(input())  # number of test cases
+
+# for _ in range(T):
+#     x, y = map(int, input().split())
+#     print(findTotal(x, y))
